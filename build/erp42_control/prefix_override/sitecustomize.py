a81a59a422dce3d20c4e5efc910227d98c68d911
jjm <whwoals159874@gmail.com>
2025-09-09 20:08:16 +0900

    del build log install

--- build/erp42_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/unita/ros2_ws/install/erp42_control'
--- build/interfaces_control_pkg/ament_cmake_python/interfaces_control_pkg/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='interfaces_control_pkg',
-    version='0.0.0',
-    packages=find_packages(
-        include=('interfaces_control_pkg', 'interfaces_control_pkg.*')),
-)
